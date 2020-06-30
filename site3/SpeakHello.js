959e074880d24a19aaf673909a0e54a9b628efa7
vaishnavifulsundar <vaishnavi.fulsundar@cumminscollege.in>
2020-06-30 15:28:38 +0530

    o

--- site3/SpeakHello.js
+++ site3/SpeakHello.js
@@ -5,8 +5,7 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-(function (window){
-	var helloSpeaker ={
+(function (window){var helloSpeaker ={
 function speak(name) {
   console.log(speakWord + " " + name);
 }
