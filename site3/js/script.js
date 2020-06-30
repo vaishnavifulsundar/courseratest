54a8a165530edd4dfe39271115086a1f0cd8e42c
vaishnavifulsundar <vaishnavi.fulsundar@cumminscollege.in>
2020-06-30 23:40:34 +0530

    p

--- site3/js/script.js
+++ site3/js/script.js
@@ -122,15 +122,15 @@ var val=function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      
-       var homeHtmlToInsertIntoMainPage = insertProperty(html,"{{randomCategoryShortName}}",{{chosenCategoryShortName}});
+       chosenCategoryShortName ="'"+chosenCategoryShortName+"'";
+       var homeHtmlToInsertIntoMainPage = insertProperty(html,"randomCategoryShortName",chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("L", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
