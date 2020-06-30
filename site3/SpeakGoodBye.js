2ba827eb9618aad420f16988f80b26ede4bd2e39
vaishnavifulsundar <vaishnavi.fulsundar@cumminscollege.in>
2020-06-30 16:29:39 +0530

    o

--- site3/SpeakGoodBye.js
+++ site3/SpeakGoodBye.js
@@ -8,11 +8,7 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
- (function (window){var byeSpeaker ={
- 	function speak(name) {
-  console.log(speakWord + " " + name);
-}
- };
+ (function (window){var byeSpeaker ={};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 
--- site3/script.js
+++ site3/script.js
@@ -34,6 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function (){
+  
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
